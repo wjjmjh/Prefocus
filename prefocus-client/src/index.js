8f6589de10e54d3a155e2309ff23450b7e87367c
wjjmjh <u6269649@anu.edu.au>
2020-08-03 15:06:35 +1000

    DEV: added Bootstrap dependency.

--- prefocus-client/src/index.js
+++ prefocus-client/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import Prefocus from "./components/Prefocus";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
