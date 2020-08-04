6f172a84b5a3e9b30289c7b372fdad590a18cc0a
wjjmjh <u6269649@anu.edu.au>
2020-08-04 17:28:48 +1000

    STY: styled day component of the calendar.

--- prefocus-client/src/components/calendar/Day.js
+++ prefocus-client/src/components/calendar/Day.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/calendar/day.scss";
 
 class Day extends React.Component {
   render() {
--- prefocus-client/src/index.js
+++ prefocus-client/src/index.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import Prefocus from "./components/Prefocus";
+import Calendar from "./components/calendar/Calendar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
+      <Calendar />
       <Prefocus />
     </div>
   );
